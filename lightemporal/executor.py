8e6224291e7ef1213c2d72113a0a0b6ec6950dbf
Antoine Rozo <antoine.rozo@gmail.com>
2025-12-06 11:04:30 +0100

    Move workflow utils as static methods in workflow class

--- lightemporal/executor.py
+++ lightemporal/executor.py
@@ -28,26 +28,3 @@ def _timestamp_for_duration(duration: int) -> float:
 def _sleep_until(timestamp: float) -> None:
     if timestamp > time.time():
         ENV['EXEC'].suspend_until(timestamp)
-
-
-def sleep(duration):
-    return _sleep_until(_timestamp_for_duration(duration))
-
-
-def wait(name: str):
-    from .models import Activity
-    from .workflow import repos, workflow
-    workflow_ctx = workflow.currents.get()[-1]
-    workflow_id = workflow_ctx['id']
-    workflow_ctx['step'] += 1
-    step = workflow_ctx['step']
-    if repos.signals.may_find_one(workflow_id, name, step):
-        return
-    ENV['EXEC'].suspend()
-
-
-def signal(workflow_id: str, name: str):
-    from .models import Signal
-    from .workflow import repos
-    repos.signals.new(Signal(workflow_id=workflow_id, name=name))
-    ENV['EXEC'].wake_up(workflow_id)
